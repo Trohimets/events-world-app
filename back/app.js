da7a4313c646fd1fe8331d527426b91c1739f98b
Константин Трохимец <katrohimets@mail.ru>
2024-02-14 16:59:02 +0300

    add parser

--- back/app.js
+++ back/app.js
@@ -3,10 +3,31 @@ import bodyParser from 'body-parser';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import {userRouter} from './routes/index.js';
+import {getData} from './parser/parser.js'
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/', userRouter);
+
+
+// Получение информации о задаче (требуется авторизация)
+app.post('/requests/', authenticateToken, async (req, res) => {
+  const search_name = req.body.search_name
+  const result = getData(search_name)
+  console.log(result)
+});
+
+// Функция для проверки авторизации
+function authenticateToken(req, res, next) {
+  const token = req.headers['authorization'];
+  if (token == null) return res.status(401).json({ error: 'Требуется авторизация' });
+
+  jwt.verify(token, 'secret', (err, user) => {
+    if (err) return res.status(403).json({ error: 'Ошибка авторизации' });
+    req.user = user;
+    next();
+  });
+}
 
 
 const PORT = 3000;
